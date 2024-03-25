0c42e812868c8cd03cebc5b4cbd4030538b4800b
Marcos Lima <araujolima728@gmail.com>
2024-03-24 22:55:58 -0300

    add alocando

---
+++ alocando/libVetor.h
@@ -0,0 +1,8 @@
+#ifndef LIBVETOR_H_INCLUDED
+#define LIBVETOR_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#endif
---
+++ alocando/main.c
@@ -0,0 +1,11 @@
+#include "libVetor.h"
+
+int main(){
+    int tam, *vetor;
+
+    printf("\nDigite a quantidade de elementos que deseja alocar: \n");
+    scanf("%d", &tam);
+    
+
+    return 0;
+}
