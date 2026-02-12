15673addd3c1f3b26274141efdddb2a095235da6
marcoslima42 <araujolima728@gmail.com>
2026-02-12 10:52:39 -0300

    add: quicksort hoare

---
+++ grokkingAlgorithms/04_quick_sort/qsHoare.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+void quicksort(vector<int> &list, int begin, int end){
+    int pivot = 
+
+}
+
+int main(){
+
+    vector<int> list = {8, 6, 1, 3, 2};
+    quicksort(); 
+    
+    return 0;
+}
